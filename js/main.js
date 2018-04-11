51ad65d823c3da7bca633e85d088d6db253873e0
Rye434 <grif0193@algonquinlive.com>
2018-04-10 22:35:01 -0400

    Added website

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var article = document.getElementById("filled");
+var h3 = document.createElement("h3");
+var p = document.createElement("p");
+
+p.textContent = "Algonquin College - Ottawa, Ontario";
+
+article.appendChild(p);
