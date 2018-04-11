a39ee517f0e96d93759c8bf0b6dcb76b43f926f3
Rye434 <grif0193@algonquinlive.com>
2018-04-11 18:10:31 -0400

    Changed whole website

--- js/main.js
+++
@@ -1,7 +0,0 @@
-var article = document.getElementById("filled");
-var h3 = document.createElement("h3");
-var p = document.createElement("p");
-
-p.textContent = "Algonquin College, Mobile Application Design & Development, September 2016 - April 2018";
-
-article.appendChild(p);
