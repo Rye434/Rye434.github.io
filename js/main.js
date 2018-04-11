bb019b2b08d63c357de856f74e41ab15af70b401
Rye434 <grif0193@algonquinlive.com>
2018-04-10 23:16:08 -0400

    Updated Style

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ var article = document.getElementById("filled");
 var h3 = document.createElement("h3");
 var p = document.createElement("p");
 
-p.textContent = "Algonquin College - Ottawa, Ontario";
+p.textContent = "Algonquin College, Mobile Application Design & Development, September 2016 - April 2018";
 
 article.appendChild(p);
